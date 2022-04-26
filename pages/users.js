9b3d9fa06452c55dd4b8ee06d2f4e4a14abb58db
alan <alanarostegui@hotmail.com>
2022-04-25 22:48:20 -0500

    feat: static generation with getStaticProps

---
+++ pages/users.js
@@ -0,0 +1,24 @@
+function UsersList({ users }) {
+  return (
+    <div className="users-list">
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default UsersList;
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
