c7e7b4277d80a80b5a6665b922cf7fe0b070f4c8
alan <alanarostegui@hotmail.com>
2022-04-26 00:46:14 -0500

    feat: running static generation builds

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Hello Next.js</h1>;
+  return (
+    <>
+      <h1>Hello Next.js</h1>
+      <Link href="/users">
+        <a>Users</a>
+      </Link>
+    </>
+  );
 }
 
 export default Home;
